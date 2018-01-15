25ede6df3bd30c948c75ba64962bbcb2cf6657a0
GIT Admin <arimon.13@gmail.com>
2018-01-15 14:00:37 -0800

    Add Hotel home page

--- src/main/webapp/angular4/src/app/app.component.ts
+++ src/main/webapp/angular4/src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  roomsearch: FormGroup;
+  rooms: Room[]  
+    ngOnInit(){
+        this.roomsearch = new FormGroup({
+            checkin : new FormControl(''),
+            checkout : new FormControl('')   
+        });
+        this.rooms = ROOMS;
+    }
+    
+    onSubmit(value){
+       console.log(value);     
+    }
+    
+    reserveRoom(value:string){
+        console.log(value);     
+   }
 }
+export interface Room {
+    id: string;
+    roomNumber: string;
+    price: string;
+    links: string;
+}
+var ROOMS: Room[] = [
+    {
+        "id": "12454",
+        "roomNumber": "201",
+        "price": "66.69",
+        "links": ""
+    }, {
+        "id": "12452",
+        "roomNumber": "202",
+        "price": "68.69",
+        "links": ""
+    }, {
+        "id": "12444",
+        "roomNumber": "203",
+        "price": "66.69",
+        "links": ""
+    }
+
+]
